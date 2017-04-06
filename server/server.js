23d736bdf1f69c32fd91f2b8077ef230f1e047be
Alex Gorelikov <gorelikov.alex@gmail.com>
2017-04-06 22:49:18 +0300

    Test post /todos route

--- server/server.js
+++ server/server.js
@@ -6,16 +6,18 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
 	var todo = new Todo({
-		text: 'Some text'
+		text: req.body.text
 	});
+	
 	todo.save().then((docs) => {
 		res.send(docs);
 	},(err) => {
-		res.status(400).send(err.errors.text.message);
+		res.status(400).send(err);
 	});
 });
 
@@ -24,6 +26,7 @@ app.post('/users', (req, res) => {
 		name: req.body.name,
 		email: req.body.email
 	});
+	
 	user.save().then((docs) => {
 		res.send(docs);
 	},(err) => {
@@ -31,6 +34,9 @@ app.post('/users', (req, res) => {
 	});
 });
 
+
 app.listen(3000, () => {
 	console.log('Started on port 3000');
 });
+
+module.exports = {app};

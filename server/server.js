44ee325d123fca7bf831967d5336f86a0117e868
Alex Gorelikov <gorelikov.alex@gmail.com>
2017-04-07 18:10:33 +0300

    GET route added

--- server/server.js
+++ server/server.js
@@ -9,6 +9,14 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.get('/todos', (req, res) => {
+	Todo.find({}).then((doc) => {
+		res.send(doc);	
+	},(err) => {
+		res.status(400).send(err);
+	})
+});
+
 app.post('/todos', (req, res) => {
 	var todo = new Todo({
 		text: req.body.text

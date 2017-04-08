4c11943cb35050bd23e048a8357844bb12110522
Alex Gorelikov <gorelikov.alex@gmail.com>
2017-04-08 12:30:20 +0300

    Data updated

--- playground/mongobd-connect.js
+++ playground/mongobd-connect.js
@@ -6,8 +6,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 		console.log('Unable to connect to mongo server');
 	}
 	console.log('Connected to mongo server');
+	
 	db.collection('Todos').insertOne({
-		                                 text     : 'Sleep',
+		                                 text     : 'leep',
 		                                 completed: false
 	                                 }, (err, result) => {
 		if(err){
@@ -16,7 +17,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	
 		console.log(JSON.stringify(result.ops, undefined, 2));
 	});
-
+	
 	//db.collection('Todos').insertOne({
 	//	                                 name    : 'Zalex',
 	//	                                 age     : 32,
@@ -27,5 +28,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	//	}
 	//	console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
 	//});
+	
+	//db.collection('Todos').insertOne({
+	//	                                 name    : 'Zalex',
+	//	                                 age     : 32,
+	//	                                 location: 'Nowhere'
+	//                                 }, (err, result) => {
+	//	if(err){
+	//		return console.log('Could not insert todo', err);
+	//	}
+	//	console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+	//});
+	
 	db.close();
 });

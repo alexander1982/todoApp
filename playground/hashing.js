d8212969e7a12483474df9af694a20890b1f28ff
Alex Gorelikov <gorelikov.alex@gmail.com>
2017-04-10 17:35:26 +0300

    Hashing added to ussaer model

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,20 @@
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+var psw = '123abc';
+
+bcrypt.genSalt(12, (err, salt) => {
+	bcrypt.hash(psw, salt, (err, hash) => {
+		console.log(hash);
+	});
+});
+
+var hashedPassword = '$2a$12$o4zcjzMyMI8ANLnNYVdJx.kQWZBmCe27YvNXPFOIEP610bv6yVtPm';
+
+bcrypt.compare(psw, hashedPassword,(err, result) => {
+	console.log(result);
+});
+
 //var message = 'Iam user num 3';
 //var hash = crypto.createHash('sha256',message).digest('hex');
 //
@@ -24,11 +39,11 @@ const jwt = require('jsonwebtoken');
 //	console.log('Data was changed.Do not trust');
 //}
 
-var data = {
-	id: 10
-};
-var token = jwt.sign(data, '123abc');
-console.log('Token: ', token);
-console.log('----------------------------');
-var decoded = jwt.verify(token, '123abc');
-console.log('Decoded: ', decoded);
+//var data = {
+//	id: 10
+//};
+//var token = jwt.sign(data, '123abc');
+//console.log('Token: ', token);
+//console.log('----------------------------');
+//var decoded = jwt.verify(token, '123abc');
+//console.log('Decoded: ', decoded);
